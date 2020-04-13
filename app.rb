75c5886de577831140758262f3c4f6b574b5ea21
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-13 19:56:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		erb :index 
-	end
-	178.128.14.28:36471
-end
+      erb :index
+    end
+ 
+    get "/info" do
+      "Testing the info page"
+    end
